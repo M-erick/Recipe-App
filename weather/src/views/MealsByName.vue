229a176cf0301149d3c06ac423c3c3ae91224482
M-erick <muriithierick3758@gmail.com>
2024-06-09 10:22:00 +0300

    created the views for each url

---
+++ weather/src/views/MealsByName.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Search by Name
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ weather/src/views/MealsByletter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Search by Letter
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
