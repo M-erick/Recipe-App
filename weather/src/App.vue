17168fd4b93e31223b68f316fd5ddce47725cdfb
M-erick <muriithierick3758@gmail.com>
2024-06-09 10:24:06 +0300

    updated the router/index.js file

--- weather/src/App.vue
+++ weather/src/App.vue
@@ -1,7 +1,27 @@
 <template>
-  <div>
+  <div  class="bg-gray-100 h-full">
+    <header class="bg-white shadow h-16 flex justify-between items-stretch">
+      <router-link :to="{name:'home'}" class="inline-flex items-center h-full px-5">
+        Home
 
-    <h1 class="bg-blue-500">Hello wolrd</h1>
+      </router-link>
+      <div class="flex items-center gap-1">
+        <router-link :to="{name:'byName'}" class="inline-flex items-center transition-colors px-2 h-full hover:bg-purple-100">
+          Search Meals
+        </router-link>
+        <router-link :to="{name:'byLetter'}" class="inline-flex items-center transition-colors px-2 h-full hover:bg-purple-100">
+         Meals by Letter
+        </router-link>
+        <router-link :to="{name:'byIngredients'}" class="inline-flex items-center transition-colors px-2 h-full hover:bg-purple-100">
+         Meals by Ingredients
+        </router-link>
+
+      </div>
+
+
+    </header>
+
+
   <router-view/>
   </div>
  
--- weather/src/router/index.js
+++ weather/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import  MealList from '../views/MealList.vue'
+import MealsByIngredient from '@/views/MealsByIngredient.vue'
+import MealsByLetter from '@/views/MealsByletter.vue'
+import MealsByName from '@/views/MealsByName.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +17,21 @@ const router = createRouter({
       path: '/letter/:letter',
       name: 'byLetter',
       component: MealList
+    },
+    {
+      path: '/by-name/:name',
+      name: 'byName',
+      component: MealsByName
+    },
+    {
+      path: '/by-letter/:letter',
+      name: 'byLetter',
+      component: MealsByLetter
+    },
+    {
+      path: '/by-ingredient/:ingredient',
+      name: 'byIngredients',
+      component: MealsByIngredient
     },
     
   ]
