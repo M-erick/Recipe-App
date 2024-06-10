52074bea5b9e693ef68b3d63df2c8e7d58612475
M-erick <muriithierick3758@gmail.com>
2024-06-10 12:26:35 +0300

    styled the skeleton

--- weather/src/components/Animations/IngredientSkeleton.vue
+++ weather/src/components/Animations/IngredientSkeleton.vue
@@ -1,8 +1,17 @@
-<template v-for="n in 10" :key="n">
-  <div class="block bg-white rounded p-3 mb-3 shadow">
-    <AnimatedPlaceholder class="h-6 w-1/2 mb-2" />
-    <!-- <AnimatedPlaceholder class="h-4 w-full" /> -->
-  </div>
+<template>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
+
+      
+      <AnimatedPlaceholder class="h-12  mb-2" />
+      <AnimatedPlaceholder class="h-12  mb-2" />
+      <AnimatedPlaceholder class="h-12  mb-2" />
+      <AnimatedPlaceholder class="h-12  mb-2" />
+      <AnimatedPlaceholder class="h-12  mb-2" />
+      <AnimatedPlaceholder class="h-12  mb-2" />
+
+    </div>
+
+
 </template>
 <script setup>
 import AnimatedPlaceholder from '@/components/AnimatedPlacehoder.vue';
