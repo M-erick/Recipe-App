356b5d5692919ae586ab4184846420ef267030cc
M-erick <muriithierick3758@gmail.com>
2024-06-10 09:58:55 +0300

    Styled Meal card component Animation

---
+++ weather/src/components/Animations/MealItemSkeleton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AnimatedPlaceholder class="rounded-t-xl h-48 w-full mb-2" />
+    <div class="px-3">
+      <AnimatedPlaceholder class="h-6 w-1/2 mb-2" />
+      <AnimatedPlaceholder class="h-4 w-full mb-4" />
+      <AnimatedPlaceholder class="h-4 w-3/4 mb-4" />
+      <AnimatedPlaceholder class="h-6 w-1/3" />
+    </div>
+  </div>
+</template>
+<script setup>
+import AnimatedPlaceholder from '@/components/AnimatedPlacehoder.vue';
+
+</script>
--- weather/src/components/MealItem.vue
+++ weather/src/components/MealItem.vue
@@ -1,31 +1,44 @@
 <template>
   <div class="bg-white shadow rounded-xl">
-    <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
-      <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl h-48 w-full object-cover">
+    <div v-if="loading">
+      <MealItemSkeleton/>
+    
+    </div>
+    <div v-else>
+      <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
+        <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl h-48 w-full object-cover">
+      </router-link>
 
-    </router-link>
-
-    <div class="px-3">
-      <h3 class="font-semibold">{{ meal.strMeal }}</h3>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut vero mollitia provident possimus.</p>
-      <div class="flex items-center justify-between">
-
-        <YouTube :href="meal.strYoutube">YouTube</YouTube>
-
+      <div class="px-3">
+        <h3 class="font-semibold">{{ meal.strMeal }}</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut vero mollitia provident possimus.</p>
+        <div class="flex items-center justify-between">
+          <YouTube :href="meal.strYoutube">YouTube</YouTube>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 import YouTube from '@/components/YouTubeButton.vue';
-const loading
-const { meal } = defineProps({
+import MealItemSkeleton from '@/components/Animations/MealItemSkeleton.vue';
+
+const { meal: mealProp } = defineProps({
   meal: {
     required: true,
     type: Object
   }
 });
-</script>
 
-<style></style>
+const loading = ref(true);
+const meal = ref({});
+
+onMounted(() => {
+  setTimeout(() => {
+    meal.value = mealProp;
+    loading.value = false;
+  }, 2000);
+});
+</script>
