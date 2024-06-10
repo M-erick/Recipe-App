23d4527d13c74db25b4e16075606bdd8c60c50d4
M-erick <muriithierick3758@gmail.com>
2024-06-10 11:57:06 +0300

    Made the navbar sticky at the top

--- weather/src/components/DefaultLayout.vue
+++ weather/src/components/DefaultLayout.vue
@@ -1,10 +1,10 @@
 <template>
- <div  >
-    <navbar/>
+  <div>
+    <navbar class="sticky top-0 z-50" />
 
-
-    <router-view/>
-    </div></template>
+    <router-view />
+  </div>
+</template>
 
 <script setup>
 import navbar from '@/components/Navbar.vue';
