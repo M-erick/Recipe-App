0cd93a8c6644f27a3b87bbfb4230af0c80cc7faa
M-erick <muriithierick3758@gmail.com>
2024-06-10 10:25:14 +0300

    added margin of the button component

--- weather/src/components/Animations/MealItemSkeleton.vue
+++ weather/src/components/Animations/MealItemSkeleton.vue
@@ -5,7 +5,7 @@
       <AnimatedPlaceholder class="h-6 w-1/2 mb-2" />
       <AnimatedPlaceholder class="h-4 w-full mb-4" />
       <AnimatedPlaceholder class="h-4 w-3/4 mb-4" />
-      <AnimatedPlaceholder class="h-6 w-1/3" />
+      <AnimatedPlaceholder class="h-6 w-1/3 m-3" />
     </div>
   </div>
 </template>
