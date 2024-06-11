c92a60365d452b93723d6e0b8e1c2bd4aba0ae83
M-erick <muriithierick3758@gmail.com>
2024-06-11 08:57:10 +0300

    Updates

--- weather/src/components/Navbar.vue
+++ weather/src/components/Navbar.vue
@@ -1,17 +1,21 @@
 <template>
-  <header class="bg-white shadow h-16 flex justify-between items-stretch">
-      <router-link :to="{name:'home'}" class="inline-flex items-center h-full px-5">
-        Home
+  <div>
 
+    <header class="bg-white shadow h-16 flex justify-between items-stretch">
+      <router-link :to="{name:'home'}" class="inline-flex items-center h-full px-5">
+        
+
+        <img src="../assets/images/cooks.png"  width="50px" height="50px" alt="">
+        
       </router-link>
       <div class="flex items-center gap-1">
-        <router-link :to="{name:'byName'}" class="inline-flex items-center transition-colors px-2 h-full hover:bg-purple-100">
+        <router-link :to="{name:'byName'}" class="inline-flex items-center transition-colors navbar__link-hover h-full ">
           Search Meals
         </router-link>
         <!-- <router-link :to="{name:'byLetter'}" class="inline-flex items-center transition-colors px-2 h-full hover:bg-purple-100">
          Meals by Letter
         </router-link> -->
-        <router-link :to="{name:'ingredients'}" class="inline-flex items-center transition-colors px-2 h-full hover:bg-purple-100">
+        <router-link :to="{name:'ingredients'}" class="inline-flex items-center transition-colors navbar__link-hover h-full ">
          Meals by Ingredients
         </router-link>
 
@@ -19,4 +23,17 @@
 
 
     </header>
+  </div>
+  
 </template>
+<script setup>
+</script>
+<style scoped>
+.navbar__link-hover {
+  padding-left:2px;
+  padding-right: 2px;
+}
+.navbar__link-hover:hover{
+  color :rgba(76,155,150,255);
+}
+</style>
--- weather/src/views/Home.vue
+++ weather/src/views/Home.vue
@@ -22,14 +22,14 @@
 
 
         <div class="border rounded">
-            
 
-            <div class="px-3 py-2 ">
-              <h3 class="font-semibold">Lorem ipsum dolor sit amet.</h3>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia.</p>
 
-            </div>
+          <div class="px-3 py-2 ">
+            <h3 class="font-semibold">Lorem ipsum dolor sit amet.</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia.</p>
+
           </div>
+        </div>
 
         <div>
         </div>
@@ -38,32 +38,32 @@
       <div class=" rounded-2xl bg-white p-5 bg-opacity-40 backdrop-filter backdrop-blur-lg">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-center px-2">
 
-          <div class="border rounded shadow group transform hover:-translate-y-2 hover:shadow-2xl  cursor-pointer ">
-            <div class="px-3 py-2">
-
-            <router-link to="/">
-              <img
-                src="https://plus.unsplash.com/premium_photo-1661902745675-11846e4f9a41?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="mealstrMeal" class="rounded-xl h-48 w-full object-cover  transition-transform duration-300 transform group-hover:scale-105">
-            </router-link>
-
-
+          <div
+            class="border rounded shadow group transform hover:-translate-y-2 hover:shadow-2xl cursor-pointer relative">
+            <div class="px-3 py-2 relative">
+              <router-link to="/">
+                <img
+                  src="https://plus.unsplash.com/premium_photo-1661902745675-11846e4f9a41?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="mealstrMeal"
+                  class="rounded-xl h-48 w-full object-cover transition-transform duration-300 transform group-hover:scale-105">
+              </router-link>
+              <i class="fas fa-heart absolute top-3 right-4 hover:right-3 bg-white/70 backdrop-blur-sm text-red-500 rounded-full p-2"></i>
             </div>
 
-            <div class="px-3 py-2 ">
+            <div class="px-3 py-2">
               <h3 class="font-semibold">Lorem ipsum dolor sit amet.</h3>
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia.</p>
-
             </div>
           </div>
           <div class="border rounded shadow group transform hover:-translate-y-2 hover:shadow-2xl  cursor-pointer ">
             <div class="px-3 py-2">
 
-            <router-link to="/">
-              <img
-                src="https://plus.unsplash.com/premium_photo-1661902745675-11846e4f9a41?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="mealstrMeal" class="rounded-xl h-48 w-full object-cover  transition-transform duration-300 transform group-hover:scale-105">
-            </router-link>
+              <router-link to="/">
+                <img
+                  src="https://plus.unsplash.com/premium_photo-1661902745675-11846e4f9a41?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="mealstrMeal"
+                  class="rounded-xl h-48 w-full object-cover  transition-transform duration-300 transform group-hover:scale-105">
+              </router-link>
 
 
             </div>
